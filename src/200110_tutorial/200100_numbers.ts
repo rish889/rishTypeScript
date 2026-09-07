b36eb613ae0a3f640b230ab6a61b6a49b729cad5
Rishabh Garcha <rish889@gmail.com>
2026-09-07 15:22:12 +0530

    message 2026_09_07_01

--- src/200110_tutorial/200100_numbers.ts
+++ src/200110_tutorial/200100_numbers.ts
@@ -1,8 +1,7 @@
 let i: number = 10;
-console.log(i);
-
 let price: number = 9.95;
-console.log(price);
-
 let big: bigint = 9007199254740991n;
+
+console.log(i);
+console.log(price);
 console.log(big);
---
+++ src/200110_tutorial/200200_string.ts
@@ -0,0 +1,8 @@
+let firstName: string = 'John';
+let title: string = "Web Developer";
+
+let description = `This TypeScript string can 
+span multiple 
+lines
+`;
+console.log(description);
