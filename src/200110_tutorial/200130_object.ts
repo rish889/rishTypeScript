a2727f8bc6a854ebc098f3ab27a1bc229d34b913
Rishabh Garcha <rish889@gmail.com>
2026-09-07 15:57:43 +0530

    message 2026_09_07_01

--- src/200110_tutorial/200130_object.ts
+++ src/200110_tutorial/200130_object.ts
@@ -1,1 +1,36 @@
 // primitive types - number, bigint, string, boolean, null, undefined, symbol
+
+let employee1: object = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 25,
+    jobTitle: 'Web Developer'
+};
+
+
+
+
+let employee2: {
+    firstName: string;
+    lastName: string;
+    age: number;
+    jobTitle: string;
+} = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 25,
+    jobTitle: 'Web Developer'
+};
+
+
+
+
+console.log(employee2);
+console.log(employee2.valueOf());
+console.log(employee2.toString());
+
+
+
+
+// The object type represents all non-primitive values while the Object type describes the functionality of all objects.
+// For example, the Object type has the toString() and valueOf() methods that can be accessible by any object.
